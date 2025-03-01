06211655a1880201222511855648d6923cf66984
Praveen Murugesan <praveen.murugesan@Praveens-MacBook-Pro.local>
2025-03-01 11:22:46 +0100

    prepare to render

--- app.py
+++ app.py
@@ -601,4 +601,4 @@ window.closeSignaturePad = () => editor.closeSignaturePad();
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()  # Remove debug=True for production
